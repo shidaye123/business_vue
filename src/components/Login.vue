8cd8da96e524778a61a782824e239301f9ccded2
shiyong <946076649@qq.com>
2019-08-29 09:22:41 +0800

    基于vue脚手架构建的项目框架，淘宝镜像cnpm+vuecli+vue

---
+++ src/components/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <input type="text" v-model="user.username" placeholder="请输入用户名"><br>
+    <input type="password" v-model="user.password" placeholder="请输入密码"><br>
+    <button @click="login">登录</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "login",
+    data(){
+      return{
+        user:{
+          username:"",
+          password:""
+        }
+      }
+    },
+    methods:{
+      login:function () {
+        this.$axios.get('http://localhost:8080/user/login.do?username='+this.user.username+'&password='+this.user.password)
+          .then(function (response) {
+            console.log(response)
+            if (response.data.status === 0){
+              alert("登录成功！")
+            } else {
+              alert("注册失败！")
+            }
+
+          })
+          .catch(function () {
+            alert("登录失败!")
+          })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Register.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+      <input type="text" v-model="userInfo.username" placeholder="用户名"><br>
+      <input type="password" v-model="userInfo.password" placeholder="密码"><br>
+      <input type="text" v-model="userInfo.email" placeholder="邮箱"><br>
+      <input type="text" v-model="userInfo.phone" placeholder="手机号"><br>
+      <input type="text" v-model="userInfo.question" placeholder="密保问题"><br>
+      <input type="text" v-model="userInfo.answer" placeholder="答案"><br>
+      <button @click="register">注册</button>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "Register",
+      data(){
+          return{
+            userInfo:{
+              username:"",
+              password:"",
+              email:"",
+              phone:"",
+              question:"",
+              answer:""
+            }
+          }
+      },
+      methods:{
+        register:function () {
+          this.$axios.get('http://localhost:8080/user/register.do?username='+this.userInfo.username+'&password='+this.userInfo.password+
+                          '&email='+this.userInfo.email+'&phone='+this.userInfo.phone+'&question='+this.userInfo.question+'&answer='+this.userInfo.answer)
+            .then(function (response) {
+              console.log(response)
+              if (response.data.status === 0){
+                alert("注册成功！")
+              } else {
+                alert("注册失败！")
+              }
+            })
+            .catch(function () {
+              alert("注册失败！")
+            })
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
